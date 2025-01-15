1c1eda7ec6ddd34e5bb00c32d6bf8361ef66ff6b
ebegcoding <aleksacollaku34@gmail.com>
2025-01-15 18:33:04 +0100

    fix: allow write

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join("db/db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
